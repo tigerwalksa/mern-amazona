87ca2118a14d330a9e07b577a613480d38b4c007
tigerwalk <tigerwalkmlondi@gmail.com>
2022-03-13 21:09:42 +0200

    video-3

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list items</main>
+    </div>
+  );
+}
+
+export default App;
